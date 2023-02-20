a85f4059db11344292c77b2a885b365d3ca540a8
kakao <coolofficials@gmail.com>
2023-02-20 17:24:28 +0900

    complete: pylint_practice

--- pylint_practice.py
+++ pylint_practice.py
@@ -7,18 +7,18 @@ SHIFT = 3
 choice = input("would you like to encode or decode?")
 word = input("Please enter text")
 LETTERS = string.ascii_letters + string.punctuation + string.digits
-ENCODED = ''
+ENCODED = ""
 if choice == "encode":
     for letter in word:
-        if letter == ' ':
-            ENCODED = ENCODED + ' '
+        if letter == " ":
+            ENCODED = ENCODED + " "
         else:
             X = LETTERS.index(letter) + SHIFT
             ENCODED = ENCODED + LETTERS[X]
 if choice == "decode":
     for letter in word:
-        if letter == ' ':
-            ENCODED = ENCODED + ' '
+        if letter == " ":
+            ENCODED = ENCODED + " "
         else:
             X = LETTERS.index(letter) - SHIFT
             ENCODED = ENCODED + LETTERS[X]
