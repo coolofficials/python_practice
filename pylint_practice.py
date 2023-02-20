09cd232d3b6613c3c5b1b63e62a346170be139f0
kakao <coolofficials@gmail.com>
2023-02-20 16:25:37 +0900

    fix: line 9

--- pylint_practice.py
+++ pylint_practice.py
@@ -6,12 +6,12 @@ word = input("Please enter text")
 letters = string.ascii_letters + string.punctuation + string.digits
 encoded = ''
 if choice == "encode":
-  for letter in word:
-      if letter == ' ':
-          encoded = encoded + ' '
-      else:
-          x = letters.index(letter) + shift
-          encoded=encoded + letters[x]
+    for letter in word:
+        if letter == ' ':
+            encoded = encoded + ' '
+        else:
+            x = letters.index(letter) + shift
+            encoded=encoded + letters[x]
 if choice == "decode":
   for letter in word:
       if letter == ' ':
